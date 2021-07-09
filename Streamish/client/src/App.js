30d353ea5b19c1606199131e417b5ef17e2a136c
Bryson Goins <brysonmg@gmail.com>
2021-07-09 11:03:17 -0500

    React with API - follow along code - starting exercises

---
+++ Streamish/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { VideoList } from './components/VideoList';
+
+function App() {
+  return (
+    <div className="App">
+      <VideoList />
+    </div>
+  );
+}
+
+export default App;
